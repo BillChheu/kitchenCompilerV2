f2eb792c408ed1af87c9e05b0129053077841471
Bill Chheu <asyoud@gmail.com>
2022-03-29 18:45:59 -0400

    adding login validation

--- utils/validation.js
+++ utils/validation.js
@@ -22,3 +22,20 @@ module.exports.validateUserRegistration = (username, email, password, confirmPas
         valid: (Object.keys(errors).length < 1)
     }
 }
+
+module.exports.validateUserLogin = (username, password) => {
+    const errors = {}
+    
+    if (username.trim() === "") {
+        errors.username = "Username must not be empty!"
+    } 
+    if (password.trim() === "") {
+        errors.email = "Password must not be empty!"
+    }
+
+    return {
+        errors,
+        valid: (Object.keys(errors).length < 1)
+    }
+    
+}
